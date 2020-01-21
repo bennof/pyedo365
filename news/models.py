36e013d40ea5760d7fe4a97ec62d62b6e6efe18a
Benjamin Benno Falkner <benno.falkner@gmail.com>
2020-01-21 18:36:31 +0100

    added calendar

--- news/models.py
+++ news/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+from datetime import date
 
-# Create your models here.
+from django.db import models
+from wagtail.snippets.models import register_snippet
+
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+
+@register_snippet
+class Date(models.Model):
+    date = models.DateField("Date", default=date.today)
+    title = models.CharField(max_length=250)
+    body = RichTextField(blank=True,max_length=500)
+    image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('date'),
+        FieldPanel('title'),
+        FieldPanel('body', classname="full"),
+        ImageChooserPanel('image'),
+    ]
+
+    @staticmethod
+    def get_next(self):
+        Date.objects.filter(date__gte=date.today()).order_by("date")[:5]
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Calendar'
--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^(?P<provider>[^/]+)/',views.login_page),
     url(r'',views.login_page,name='login'),
+    url(r'^cms/login', views.login_page, name='wagtailadmin_login'),
+    url(r'^logout/$', views.login_page, name='wagtailadmin_logout'),
 ]
